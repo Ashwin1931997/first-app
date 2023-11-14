95347a42145abfa24c36f907576cc0244caa44e4
Ashwin1931997 <ashwinarya101@gmail.com>
2023-11-14 15:26:40 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/offer.css';
+import Offer from './components/Offer';
+
+function App() {
+  return (
+    <div className="App">
+     <Offer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Offer.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function Offer() {
+  return (
+    
+   
+    <div>
+        <h1>RESUME</h1>
+        <div class="img">
+        <img src="ash.jpg" alt="Profile Image" width="150" height="150"></img>
+    <h2>Ashwin Kumar</h2>
+    
+    <h3>Email:ashwinarya101@gmail.com</h3>
+   
+    <h3>Phone:7760929321</h3>
+  
+    <h2>Skills</h2>
+        <p id="skills" align="center">
+        JavaScript<br></br>
+        HTML5<br></br>
+        CSS<br></br>
+        React<br></br>
+      
+    </p>
+
+    <h2>Projects</h2>
+    <p id="projects">
+        <li><strong>Project 1:</strong> The page is divided into three main sections: header, products, and cart.
+The "header" contains the title of the shopping site.
+The "products" section contains featured product listings with images, descriptions, prices, and an "Add to Cart" button.
+The "cart" section displays the items added to the shopping cart along with a total and a "Checkout" button.
+A simple "footer" with copyright information is added.</li>
+       
+    </p>
+
+    <h2>Education</h2>
+    <p id="education">Bachelor of Science in Computer Science</p>
+
+    <h2>Hobbies</h2>
+    <p id="hobbies" align="center">
+        Cricket<br></br>
+        mobile games<br></br>
+        volleyball<br></br>
+    </p>
+    </div>
+    </div>
+  )
+}
